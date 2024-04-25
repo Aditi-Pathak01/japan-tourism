d04d63f5c99ceafe1371fa691b07c05cc0e57534
Aditi-Pathak01 <pathakaditi322@gmail.com>
2024-04-25 22:06:23 +0530

    menu btn added

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 let title = document.querySelector("#page-1 h1 span");
 let nav = document.querySelector("nav");
 let backToTop = document.querySelector("#page-1 i");
+let navScroll = document.querySelector("#nav-scroll");
 
 document.addEventListener("DOMContentLoaded", () => {
   title.style.transform = "translateY(20px)";
 });
 
 document.addEventListener("scroll", () => {
-  if (scrollY > 300) {
-    nav.classList.add("nav-scroll");
-    nav.childNodes[3].innerHTML = "<i class='ri-menu-add-fill'></i>Menu";
+  if (scrollY > 0) {
+    nav.style.opacity = "0";
     backToTop.style.opacity = "1";
+    navScroll.style.transform = "translate(-50%) scale(1)"
   } else {
-    nav.classList.remove("nav-scroll");
-    nav.childNodes[3].innerText = "Home";
+    nav.style.opacity = "1";
     backToTop.style.opacity = "0";
+    navScroll.style.transform = "translate(-50%) scale(0)"
   }
 });
 backToTop.addEventListener("click", () => {
   document.documentElement.scrollTop = 0;
 });
+let click = 0
+navScroll.addEventListener("click",()=>{
+  if(click ===0){
+    navScroll.innerHTML = ""
+    navScroll.style.height = "20vh"
+    navScroll.style.width = "40vw"
+    click = 1
+  } else {
+    navScroll.innerHTML = '<i class="ri-menu-add-line"></i><span>Menu</span>'
+    navScroll.style.height = "8vh"
+    navScroll.style.width = "10vw"
+    click = 0
+  }
+})
