9c5b03de481040efe62a35cd8dfe14fa63afd8e8
Aditi-Pathak01 <pathakaditi322@gmail.com>
2024-04-26 20:46:22 +0530

    page-3 added

--- script.js
+++ script.js
@@ -1,37 +1,97 @@
-let title = document.querySelector("#page-1 h1 span");
+let title = document.querySelector("#title");
 let nav = document.querySelector("nav");
-let backToTop = document.querySelector("#page-1 i");
+let backToTop = document.querySelector("#top-btn");
 let navScroll = document.querySelector("#nav-scroll");
+let hoverElems = document.querySelectorAll(".elem-h");
 
 document.addEventListener("DOMContentLoaded", () => {
-  title.style.transform = "translateY(20px)";
+  var tl = gsap.timeline();
+  tl.from("#page-1", {
+    scale: 0,
+    duration: 1.8,
+  });
+  tl.from(nav, {
+    scale: 0,
+    duration: 1,
+  });
+  tl.to("#title", {
+    y: 20,
+  });
 });
 
 document.addEventListener("scroll", () => {
   if (scrollY > 0) {
-    nav.style.opacity = "0";
-    backToTop.style.opacity = "1";
-    navScroll.style.transform = "translate(-50%) scale(1)"
+    gsap.to(nav, {
+      opacity: 0,
+    });
+    gsap.to(backToTop, {
+      opacity: 1,
+    });
+    gsap.to(navScroll, {
+      scale: 1,
+    });
   } else {
-    nav.style.opacity = "1";
-    backToTop.style.opacity = "0";
-    navScroll.style.transform = "translate(-50%) scale(0)"
+    gsap.to(nav, {
+      opacity: 1,
+    });
+    gsap.to(backToTop, {
+      opacity: 0,
+    });
+    gsap.to(navScroll, {
+      scale: 0,
+    });
   }
 });
-backToTop.addEventListener("click", () => {
+backToTop.addEventListener("click", (scrollX) => {
   document.documentElement.scrollTop = 0;
 });
-let click = 0
-navScroll.addEventListener("click",()=>{
-  if(click ===0){
-    navScroll.innerHTML = ""
-    navScroll.style.height = "20vh"
-    navScroll.style.width = "40vw"
-    click = 1
-  } else {
-    navScroll.innerHTML = '<i class="ri-menu-add-line"></i><span>Menu</span>'
-    navScroll.style.height = "8vh"
-    navScroll.style.width = "10vw"
-    click = 0
-  }
+
+
+navScroll.addEventListener("click", () => {
+  gsap.to(navScroll, {
+    height: "40vh",
+    width: "20vw",
+  });
+  let clutter =
+    "<div class=menu-opts><a>Culture</a><a>Destination</a><a>Food</a><a>NightLife</a><a>Accomodation</a></div>";
+  navScroll.innerHTML = clutter;
+
+let culture = navScroll.childNodes[0].childNodes[0]
+culture.addEventListener("click",()=>{
+ culture.setAttribute("href","#page-3")
 })
+});
+
+navScroll.addEventListener("mouseleave", () => {
+  gsap.to(navScroll, {
+    height: "8vh",
+    width: "10vw",
+  });
+  navScroll.innerHTML = '<i class="ri-menu-add-line"></i><span>Menu</span>';
+});
+
+hoverElems.forEach((elem) => {
+  elem.addEventListener("mouseenter", () => {
+    gsap.to(elem.childNodes[3], {
+      scale: 1,
+    });
+  });
+
+  elem.addEventListener("mousemove", (dets) => {
+    gsap.to(elem.childNodes[3], {
+      x: dets.x - elem.getBoundingClientRect().x + "px",
+      y: dets.y - elem.getBoundingClientRect().y - 40 + "px",
+    });
+  });
+
+  elem.addEventListener("mouseleave", () => {
+    gsap.to(elem.childNodes[3], {
+      scale: 0,
+    });
+  });
+  elem.addEventListener("click", () => {
+    gsap.to(".elem-h", {
+      y: 10,
+    });
+  });
+});
